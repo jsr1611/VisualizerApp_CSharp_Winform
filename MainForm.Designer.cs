761b641803ed3f24c55eaa09e9e4522f1a2f039b
Jumanazar Saidov <jsr1611@outlook.com>
2020-11-17 09:53:18 +0900

    Files reorganized and optimized to avoid duplications.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace VisualizerApp
+﻿namespace DataVisualizerApp
 {
     partial class MainForm
     {
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VisualizerApp_3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DataVisualizerApp")]
+[assembly: AssemblyDescription("Sensor data visualization application for monitoring clean room environments.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("VisualizerApp_3")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyCompany("JIMSLAB Inc.")]
+[assembly: AssemblyProduct("DataVisualizerApp")]
+[assembly: AssemblyCopyright("Copyright © JIMSLAB Inc. 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
