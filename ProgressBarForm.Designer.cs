509c9c79eed16101c80732cf7fb218829cb4ca93
Jumanazar Saidov <jsr1611@outlook.com>
2020-12-03 18:01:29 +0900

    Splash screen implemented

---
+++ ProgressBarForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace DataVisualizerApp
+{
+    partial class ProgressBarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_loading = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_loading
+            // 
+            this.label_loading.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label_loading.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_loading.Location = new System.Drawing.Point(3, 59);
+            this.label_loading.Name = "label_loading";
+            this.label_loading.Size = new System.Drawing.Size(591, 54);
+            this.label_loading.TabIndex = 3;
+            this.label_loading.Text = "LOADING...";
+            this.label_loading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = global::DataVisualizerApp.Properties.Resources._30;
+            this.label1.Location = new System.Drawing.Point(8, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(586, 55);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ProgressBarForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(595, 226);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_loading);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ProgressBarForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label_loading;
+        private System.Windows.Forms.Label label1;
+    }
+}
